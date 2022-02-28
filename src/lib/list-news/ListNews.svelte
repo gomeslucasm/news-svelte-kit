672c4946ce027484428a3583d865a72eb0bfb589
Lucas Gomes <lucas.gomes@yungas.com.br>
2022-02-28 18:40:38 -0300

    loading component when is fetching

--- src/lib/list-news/ListNews.svelte
+++ src/lib/list-news/ListNews.svelte
@@ -5,6 +5,7 @@
 <script>
   import { makeUrl } from '$lib/utils'
   import { NewsItem } from '$lib/news-item'
+  import { Loading } from '$lib/loading'
   import { onMount } from 'svelte'
 
   const url = makeUrl('news/others')
@@ -17,20 +18,45 @@
     getNews()
   })
 
-  let news = []
+  let news
 </script>
 
-<div class="list">
-  {#each news as { title, category, slug, id }}
-    <div class="item">
-      <NewsItem {title} {category} {slug} {id} />
+<div class="list-container">
+  {#if news}
+    <div class="list">
+      {#each news as { title, category, slug, id }}
+        <div class="item">
+          <NewsItem {title} {category} {slug} {id} />
+        </div>
+      {/each}
     </div>
-  {/each}
+  {:else}
+    <div class="loading">
+      <Loading />
+      <span class="loading-text"> Carregando notícias </span>
+    </div>
+  {/if}
 </div>
 
 <style>
-  .list {
+  .loading-text {
+    margin: auto;
+    margin-left: 5px;
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 26px;
+    letter-spacing: 0em;
+    text-align: left;
+  }
+
+  .loading {
+    display: flex;
+  }
+  .list-container {
     margin-top: 4rem;
+  }
+  .list {
     display: grid;
     grid-template-columns: 25% 25% 25% 25%;
     width: 100%;
