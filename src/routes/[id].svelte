cbcfc3fbc32e0154dbd475b26d1beefbff53163c
Lucas Gomes <lucas.gomes@yungas.com.br>
2022-02-28 01:14:54 -0300

    start detail page + return button

---
+++ src/routes/[id].svelte
@@ -0,0 +1,20 @@
+<a href="/" id="return-button">
+  <img id="arrow" src="arrow-left.svg" alt="</div>" />
+</a>
+
+<style>
+  #return-button {
+    width: 54px;
+    height: 54px;
+    background-color: black;
+    position: absolute;
+    top: 0px;
+    display: flex;
+    cursor: pointer;
+  }
+  #arrow {
+    width: 24px;
+    height: 24px;
+    margin: auto;
+  }
+</style>
