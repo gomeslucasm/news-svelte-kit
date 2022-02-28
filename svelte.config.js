39dd2f04a25733470e93ecd96fc5119b83d31cde
Lucas Gomes <lucas.gomes@yungas.com.br>
2022-02-28 18:39:59 -0300

    finish config

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess'	
+import adapter from '@sveltejs/adapter-auto'
+import preprocess from 'svelte-preprocess'
 import autoprefixer from 'autoprefixer'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
-	},
-	preprocess: preprocess({
-		postcss: {
-			plugins: [
-				autoprefixer,
-			],
-		},
-	}),
-};
+  kit: {
+    adapter: adapter(),
+  },
+}
 
-export default config;
+export default config
