93144db165881d9a67e249bdcdbc2bf0cb6585b6
Lucas Gomes <lucas.gomes@yungas.com.br>
2022-02-28 18:39:52 -0300

    formating

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,42 +1,40 @@
 <script>
-	import '../app.css'
+  import '../app.css'
 </script>
 
 <div id="header">
-	<div id="return-button">
-		<slot name='return-button' />
-	</div>
-	<span> SVEN NEWS </span>
+  <div id="return-button">
+    <slot name="return-button" />
+  </div>
+  <span> SVEN NEWS </span>
 </div>
 
-<slot/>
-
-
+<slot />
 
 <style>
-	#header {
-		height: 54px;
-		width: 100%;
-		text-align: center;
-		position: relative;
-		background-color: #ffbd14;
+  #header {
+    height: 54px;
+    width: 100%;
+    text-align: center;
+    position: relative;
+    background-color: #ffbd14;
     display: flex;
     margin-bottom: 1rem;
-	}
-	#return-button {
-		text-align: center;
-		height: 100%;
-		width: 54px;
-		position: absolute;
-		left: 0px;
+  }
+  #return-button {
+    text-align: center;
+    height: 100%;
+    width: 54px;
+    position: absolute;
+    left: 0px;
     display: flex;
     flex-direction: column;
     justify-content: center;
     cursor: pointer;
-	}
-  span{
+  }
+  span {
     margin: auto;
     font-weight: 700;
-    line-height:29.9px;
+    line-height: 29.9px;
   }
 </style>
