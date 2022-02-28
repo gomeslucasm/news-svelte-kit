e2d72a179c8b97c9dbc408ddf5ef84c7a79b225b
Lucas Gomes <lucas.gomes@yungas.com.br>
2022-02-28 18:41:24 -0300

    removing unnused import

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,4 @@
 import adapter from '@sveltejs/adapter-auto'
-import preprocess from 'svelte-preprocess'
-import autoprefixer from 'autoprefixer'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
